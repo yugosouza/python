78de99c4a0b9a6345c79bd719bc7ee31d3844a3c
yugoSouza <yugos82@gmail.com>
2022-12-09 00:53:41 -0300

    Commit inicial

---
+++ Ex01.py
@@ -0,0 +1,11 @@
+prod1 = 'Computador'
+prod2 = 'tv'
+preco1 = 2100.50
+preco2 = 1830.0
+idade = 30
+codigo = 5291
+genero = "F"
+
+print('Produtos: \nO produto', prod1, 'custa R$', preco1)
+print(f'O produto {prod2} custa R${preco2} \nCodigo = 5291')
+print(f'Dados da pessoa: \nGenero: {genero}  idade: {idade} anos')
